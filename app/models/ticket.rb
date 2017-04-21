d66ce9a334ae178c56f7eb41e2e4982ce2010ba6
Vincent Gutierrez <vincentgg89@gmail.com>
2017-04-21 13:50:51 -0500

    Implement creating tickets for a project

---
+++ app/models/ticket.rb
@@ -0,0 +1,7 @@
+class Ticket < ApplicationRecord
+  belongs_to :project
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :description, presence: true, length: { minimum: 10 }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170412221155) do
+ActiveRecord::Schema.define(version: 20170421181204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,4 +22,14 @@ ActiveRecord::Schema.define(version: 20170412221155) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "tickets", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "project_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["project_id"], name: "index_tickets_on_project_id", using: :btree
+  end
+
+  add_foreign_key "tickets", "projects"
 end
