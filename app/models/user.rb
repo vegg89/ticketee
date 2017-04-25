387c21908218daf22b5b95bd04929ad1816b2d2c
Vincent Gutierrez <vincentgg89@gmail.com>
2017-04-25 00:59:46 -0500

    Add feature for archiving users, including protection against self-archiving

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  scope :excluding_archived, lambda { where(archived_at: nil) }
+
+  def archive
+    self.update(archived_at: Time.now)
+  end
 
   def to_s
     "#{email} (#{admin? ? "Admin" : "User"})"
