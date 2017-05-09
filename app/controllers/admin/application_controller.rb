4145884c3923dd25318006b0dbcf8a676720e91a
Vincent Gutierrez <vincentgg89@gmail.com>
2017-05-09 18:05:40 -0500

    Verify that we use authorization in all controllers

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,4 +1,5 @@
 class Admin::ApplicationController < ApplicationController
+  skip_after_action :verify_authorized, :verify_policy_scoped
   before_action :authorize_admin!
 
   def index
@@ -7,7 +8,7 @@ class Admin::ApplicationController < ApplicationController
   private
   def authorize_admin!
     authenticate_user!
-    
+
     unless current_user.admin?
       redirect_to root_path, alert: "You must be an admin to do that."
     end
